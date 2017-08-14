2a259d02bb2482f2289523052a1511d831eb346b
Xing Liu <xingliu@utexas.edu>
2017-08-13 22:20:31 -0500

    feat: set database, table, col names in php file

--- ajax_refresh.php
+++ ajax_refresh.php
@@ -5,12 +5,12 @@ define ('DBPASS','root');
 define ('DBNAME','test');
 
 function connect() {
-    return new PDO('mysql:host=192.168.1.4;dbname=test;port=3306', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    return new PDO('mysql:host=192.168.1.4;dbname=ngram;port=3306', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 }
 
 $pdo = connect();
 $keyword = $_POST['keyword'].'%';
-$sql = "SELECT * FROM output WHERE starting_phrase LIKE (:keyword) ORDER BY count DESC LIMIT 0, 10";
+$sql = "SELECT * FROM rFreq WHERE starting_phrase LIKE (:keyword) ORDER BY relative_freq DESC LIMIT 0, 10";
 $query = $pdo->prepare($sql);
 $query->bindParam(':keyword', $keyword, PDO::PARAM_STR);
 $query->execute();
@@ -18,7 +18,8 @@ $list = $query->fetchAll();
 foreach ($list as $rs) {
 	// put in bold the written text
 	//$country_name = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $rs['following_word']);
-	$predictor = $rs['starting_phrase'] . ' ' . $rs['following_word'];
+	//$predictor = $rs['starting_phrase'] . ' ' . $rs['following_word'];
+	$predictor = $rs['following_word'];
 	// add new option
     echo '<li onclick="set_item(\''.str_replace("'", "\'", $rs['following_word']).'\')">'.$predictor.'</li>';
 }
